e21051a78a4194304b6f7f39bdb019db76d48ef2
HosMercury <hos.mercury@gmail.com>
2022-06-06 01:49:35 +0200

    tried ejs and failed

--- index.js
+++ index.js
@@ -34,19 +34,14 @@ app.use(
   })
 );
 
-// // view-engine
-// const { engine } = require('express-handlebars');
-// app.engine(
-//   '.hbs',
-//   engine({ extname: '.hbs', layoutsDir: path.join(__dirname, 'views/layouts') })
-// );
-// app.set('view engine', '.hbs');
-// app.set('views', './views');
-
-const ejs = require('ejs');
-ejs.delimiter = '?';
-ejs.openDelimiter = '[';
-ejs.closeDelimiter = ']';
+// view-engine
+const { engine } = require('express-handlebars');
+app.engine(
+  '.hbs',
+  engine({ extname: '.hbs', layoutsDir: path.join(__dirname, 'views/layouts') })
+);
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
