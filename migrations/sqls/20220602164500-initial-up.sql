4cd551a7e52001fdd092c6346f48777393854d28
HosMercury <hos.mercury@gmail.com>
2022-06-13 01:31:08 +0200

    back validation finished

--- migrations/sqls/20220602164500-initial-up.sql
+++ migrations/sqls/20220602164500-initial-up.sql
@@ -1,10 +1,11 @@
 create table users (
-	id int primary key not null auto_increment,
+	id int unsigned  primary key not null auto_increment,
 	firstname varchar(50) not null,
 	lastname varchar(50) not null,
 	username varchar(50) not null unique,
 	password varchar(100) not null,
-	created_at datetime default now()
+	created_at datetime default now(),
+	updated_at datetime default now()
 );
 
 create table session (
@@ -14,26 +15,42 @@ create table session (
 );
 
 create table locals (
-	id int primary key not null auto_increment,
+	id int unsigned  primary key not null auto_increment,
 	language varchar(50) not null,
 	abbreviation varchar(10) not null unique,
 	direction enum ('ltr','rtl') default 'ltr',
-	created_at datetime default now()
+	created_at datetime default now(),
+	updated_at datetime default now()
+);
+
+create table categories (
+	id int unsigned  primary key not null auto_increment,
+	name varchar(255) not null,
+	description varchar(255),
+	parent int unsigned ,
+	created_at datetime default now(),
+	updated_at datetime default now()
 );
 
 create table attributes (
-	id int primary key not null auto_increment,
-	type varchar(50) not null,
+	id int unsigned  primary key not null auto_increment,
+	type varchar(255) not null,
+	name varchar(255) not null,
+	en_label varchar(255) not null,
+	description varchar(255),
 	required boolean not null default false,
-	min varchar(50),
-  max varchar(50),
-  unit varchar(50),
-	created_at datetime default now()
+	default_value varchar(255),
+	min int unsigned ,
+  max int unsigned ,
+  unit varchar(255),
+	created_at datetime default now(),
+	updated_at datetime default now()
 );
 
 create table attr_options (
-	id int primary key not null auto_increment,
-  attribute_id int not null references attributes(id) on delete cascade,
+	id int unsigned  primary key not null auto_increment,
+  attribute_id int unsigned  not null references attributes(id) on delete cascade,
 	value varchar(50) not null,
-	created_at datetime default now()
+	created_at datetime default now(),
+	updated_at datetime default now()
 );
