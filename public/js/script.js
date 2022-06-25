9044800e1e50906224c495f6264be2dfdd45b175
HosMercury <hos.mercury@gmail.com>
2022-06-25 18:36:39 +0200

    fuck

--- public/js/script.js
+++ public/js/script.js
@@ -288,4 +288,13 @@ $(document).ready(function () {
       container.slideUp(300);
     }
   });
+
+  $(document).mouseup(function (e) {
+    var container = $('.nex-modal');
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      container.slideUp(300);
+    }
+  });
 });
