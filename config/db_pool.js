f7f699c9eef72f8f37ebfe2e928b154a420e676e
HosMercury <hos.mercury@gmail.com>
2022-06-25 16:06:25 +0200

    adjust height

--- config/db_pool.js
+++ config/db_pool.js
@@ -15,12 +15,12 @@ async function asyncFunction() {
   try {
     conn = await pool.getConnection();
     const rows = await conn.query('SELECT 1 as val');
-    //console.log(rows); //[ {val: 1}, meta: ... ]
-    // const res = await conn.query('INSERT INTO myTable value (?, ?)', [
-    //   1,
-    //   'mariadb'
-    // ]);
-    // console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+    console.log(rows); //[ {val: 1}, meta: ... ]
+    const res = await conn.query('INSERT INTO myTable value (?, ?)', [
+      1,
+      'mariadb'
+    ]);
+    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
   } catch (err) {
     throw err;
   } finally {
